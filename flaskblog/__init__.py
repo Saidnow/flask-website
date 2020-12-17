959bb2f58643c2d7c5faef66642fc310a932fb2c
saidnow <naouis1991@gmail.com>
2020-12-17 16:30:29 +0100

    This is my last version of flask webapp

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,13 +2,38 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '4e2dcf95b691b51c2b08997f68dc63c1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login' # login is function
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login' # login is function
 login_manager.login_message_category = 'info' # info : information alerte
-from flaskblog import route
+mail = Mail()
+
+#Reason behind app create function is :
+#1.Testing. tests multiple instances of the application with different settings
+#2.Run different version of the application, Prod DEv, Test in the same time if you want
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    from flaskblog.errors.handlers import errors
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+
+    return app
